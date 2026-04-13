2c79d10723fc440f458552c5f12fe2b78ed1947f
Piyush Bhuyan <mrpiyush0007@gmail.com>
2026-04-13 21:09:38 +0530

    fix: network fix between frontend, worker and backend container

--- backend/main.py
+++ backend/main.py
@@ -35,7 +35,7 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
